0fc11a33999812725cb8328da85e8eec65ceeb6b
Dima <mr.smith8263@gmail.com>
2026-05-22 15:05:40 +0200

    UDEMY-30 | req and res

--- node-first-server/app.js
+++ node-first-server/app.js
@@ -1,10 +1,14 @@
 // const http = require('http');
 // http.createServer();
 
-import { createServer } from 'http';
+import { createServer } from "http";
 
 const server = createServer((req, res) => {
-  console.log("Request received", req);
+  console.log("Request received", req.url, req.method, req.headers);
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html><body><h1>Hello World</h1></body></html>");
+  res.end();
 });
 
 server.listen(3000);
